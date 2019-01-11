5e81ad417761ada4f9282064e776dc7876bf4df0
sieuwe elferink <sieuwe_crossen@hotmail.com>
2019-01-11 23:10:04 +0100

    fixed relay

--- 3 Mind Control Projects/Relay Control/RelayControl Arduino/Moodlight.ino
+++ 3 Mind Control Projects/Relay Control/RelayControl Arduino/Moodlight.ino
@@ -1,35 +1,43 @@
 
+int RValue;
+int GValue;
+int BValue;
+bool serialActive = false;
+String input = "";
+
+
 void setup() {
   // put your setup code here, to run once:
 
   Serial.begin(115200);
 
   pinMode(7, OUTPUT);
-
-
 }
 
 void loop() {
+  
+    if (Serial.available() > 0)
+    {
+      input = Serial.readString();
+      
+      Serial.println(input);
+      
+      int ind1 = input.indexOf(';');  
+      RValue = input.substring(0, ind1).toInt();   
+      int ind2 = input.indexOf(';', ind1+1 );  
+      GValue = input.substring(ind1+1, ind2+1).toInt();   
 
-  if (Serial.available() > 0)
-  {
-
-    if(Serial.readString() == "Aan") {
-
-    digitalWrite(7, HIGH);
-
+   if(RValue == 1){
+digitalWrite(7, HIGH);
+      
     }
 
-    else {
-
+    else
+    {
       digitalWrite(7, LOW);
-
     }
+    }
+
+ 
 
   }
-
-}
-
-
-
-
