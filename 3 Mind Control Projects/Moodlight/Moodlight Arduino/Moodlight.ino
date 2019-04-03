89f38772a0b4665b55b90fdd55e7ce73c9eb791d
sieuwe elferink <sieuwe_crossen@hotmail.com>
2019-04-03 10:02:52 +0200

    fixed moodlight

--- 3 Mind Control Projects/Moodlight/Moodlight Arduino/Moodlight.ino
+++ 3 Mind Control Projects/Moodlight/Moodlight Arduino/Moodlight.ino
@@ -2,7 +2,7 @@ int RValue;
 int GValue;
 int BValue;
 bool serialActive = false;
-String input = "";
+
 
 
 void setup() {
@@ -12,39 +12,54 @@ void setup() {
 
   pinMode(8, OUTPUT);
   pinMode(9, OUTPUT);
-  pinMode(10, OUTPUT);
   pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(4, OUTPUT);
+
+  digitalWrite(4,LOW);
 
 }
 
 void loop() {
-  
+  // put your main code here, to run repeatedly:
+  serialActive =  true;
+/*
+  while (serialActive == false)
+  {
+    digitalWrite(8, LOW);
+    if (Serial.readString() == "remote") {
+      serialActive = true;
+      Serial.println("Hi");
+    }
+  }
+  */
+while(serialActive == true){
+   digitalWrite(8, HIGH);
+
+  while (Serial.available() > 0) {
+   
+    
     if (Serial.available() > 0)
     {
-      input = Serial.readString();
-      
-      Serial.println(input);
-      
+      String input = Serial.readString();
+
       int ind1 = input.indexOf(';');  
       RValue = input.substring(0, ind1).toInt();   
       int ind2 = input.indexOf(';', ind1+1 );  
       GValue = input.substring(ind1+1, ind2+1).toInt();   
+      int ind3 = input.indexOf(';', ind2+1 );
+      BValue = input.substring(ind2+1, ind3+1).toInt();
 
-   if(RValue == 1){
-digitalWrite(7, HIGH);
       
+
     }
 
-    else
-    {
-      digitalWrite(7, LOW);
-    }
-    }
-
- 
+    analogWrite(9,RValue);
+    analogWrite(11,GValue);
+    analogWrite(12,BValue);
 
   }
+}
 
 
-
-
+}
